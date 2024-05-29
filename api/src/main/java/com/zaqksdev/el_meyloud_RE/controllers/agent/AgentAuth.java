81171c3cdebb2c1b3e57ef2340760eb003d7440e
zak <imzakbro@gmail.com>
2024-05-29 02:40:30 +0100

    final

--- api/src/main/java/com/zaqksdev/el_meyloud_RE/controllers/agent/AgentAuth.java
+++ api/src/main/java/com/zaqksdev/el_meyloud_RE/controllers/agent/AgentAuth.java
@@ -19,6 +19,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
+
 @Controller
 @RequestMapping("agent/")
 public class AgentAuth {
